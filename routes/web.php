42e27956513c736024170856787217deee818af2
HamzahJomaa <hamzah.m.jomaa@gmail.com>
2025-12-11 11:07:59 +0000

    merged correctly

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\ActivityController;
 
-    ReportingImportController
-   
-};
+
 Route::get('/', function () {
     return view('welcome');
 });
